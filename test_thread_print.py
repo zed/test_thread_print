87038adcd7bab8e1b967a2ad6d4d595fbc7832ff
zed <arn.zart+zed@gmail.com>
2011-10-07 18:55:09 +0400

    add sleep() to avoid burning CPU

--- test_thread_print.py
+++ test_thread_print.py
@@ -4,6 +4,7 @@
   $ while ! python example.py 2>&1|tee out|grep '^$';do echo -n .;done;cat out'
 """
 import os, signal, sys
+import time
 from subprocess import Popen
 from timeit import default_timer as timer 
 
@@ -22,4 +23,5 @@ while p.poll() is None:
         os.kill(p.pid, signal.SIGKILL) # compatibility with 2.4
         os.waitpid(-1, os.WNOHANG)
         sys.exit(0) # It might be OK
+    time.sleep(.1)
 assert p.returncode, "empty line encountered"
