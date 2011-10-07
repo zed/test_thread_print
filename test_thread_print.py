f9d72965ab5498e76915db8b5d175d2f54a1fc6a
zed <arn.zart+zed@gmail.com>
2011-10-07 18:50:35 +0400

    kill subprocess on timeout

---
+++ test_thread_print.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""Run in Python the command with timeout:
+
+  $ while ! python example.py 2>&1|tee out|grep '^$';do echo -n .;done;cat out'
+"""
+import os, signal, sys
+from subprocess import Popen
+from timeit import default_timer as timer 
+
+timeout = 30
+cmd = """while ! %(python)s example.py 2>&1|tee out |grep '^$'
+do echo -n .
+done
+cat out
+""" % dict(python=sys.executable)
+p = Popen(cmd, shell=True)
+
+# kill the process on timeout
+start = timer()
+while p.poll() is None:
+    if (timer() -  start) > timeout:
+        os.kill(p.pid, signal.SIGKILL) # compatibility with 2.4
+        os.waitpid(-1, os.WNOHANG)
+        sys.exit(0) # It might be OK
+assert p.returncode, "empty line encountered"
